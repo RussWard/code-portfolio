8da7e4c47548663e0e2f63761c5f99ac180e3003
Russ Ward <maddogward@gmail.com>
2017-11-18 17:35:17 -0800

    ome styling tweaks to the headline component

--- src/components/headline-styles.js
+++ src/components/headline-styles.js
@@ -2,7 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 
 export const HeadlineStyle = styled.h2`
-  height: 150px;
+  overflow: auto;
+  height: auto;
   margin: 0 10px 0 10px;
   color: #555;
   border-bottom: 2px solid;
